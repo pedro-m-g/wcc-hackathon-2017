221d21ae32b56dbabf21e3852c02d17f36b0cbf5
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail.com>
2017-10-07 00:15:30 -0700

    Set up Server

---
+++ ModelServer/src/main/java/com/mycompany/modelserver/Server.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.modelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author pedro
+ */
+@RestController
+@EnableAutoConfiguration
+public class Server {
+    
+    public static void main(String... args) {
+        SpringApplication.run(Server.class, args);
+    }
+    
+    @PostMapping("/")
+    public String hello() {
+        return "Hola";
+    }
+    
+}
